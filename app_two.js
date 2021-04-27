ffd7773b4baa915dedd504cf723ce6eff7d49f59
ROBIN OCHOA <robin8a@local.mac>
2021-04-27 08:47:41 -0500

    update

---
+++ app_two.js
@@ -0,0 +1,35 @@
+var awsIot = require('aws-iot-device-sdk');
+
+//
+// Replace the values of '<YourUniqueClientIdentifier>' and '<YourCustomEndpoint>'
+// with a unique client identifier and custom host endpoint provided in AWS IoT.
+// NOTE: client identifiers must be unique within your AWS account; if a client attempts
+// to connect with a client identifier which is already in use, the existing
+// connection will be terminated.
+//
+var device = awsIot.device({
+    keyPath: 'certs/kio-smart-contracts-iot-device/kio-smart-contracts-iot-device.private.key',
+   certPath: 'certs/kio-smart-contracts-iot-device/kio-smart-contracts-iot-device.cert.pem',
+     caPath: 'certs/kio-smart-contracts-iot-device/root-CA.crt',
+   clientId: 'kio-smart-contracts-iot-device',
+       host: 'az6wto8a6h0jn-ats.iot.us-east-1.amazonaws.com',
+     region: 'us-east-1',
+       port: 8883,
+      debug: true
+ });
+
+//
+// Device is an instance returned by mqtt.Client(), see mqtt.js for full
+// documentation.
+//
+device
+  .on('connect', function() {
+    console.log('connect');
+    device.subscribe('topic_1');
+    device.publish('topic_2', JSON.stringify({ test_data: 1}));
+  });
+
+device
+  .on('message', function(topic, payload) {
+    console.log('message', topic, payload.toString());
+  });
