c19bd22f09d7296c92e2278062c086de432683d3
ROBIN OCHOA <robin8a@local.mac>
2021-09-01 14:46:58 -0500

    imports + cardano cli + piñata

--- app.js
+++ app.js
@@ -1,6 +1,55 @@
 const deviceModule = require('aws-iot-device-sdk').device;
 const cmdLineProcess = require('aws-iot-device-sdk/examples/lib/cmdline');
 
+
+var awsIot = require('aws-iot-device-sdk');
+var shell = require('shelljs');
+const pinataSDK = require('@pinata/sdk');
+var AWS = require('aws-sdk');
+const assets = require("./assets.json")
+const cmdLineProcess = require('aws-iot-device-sdk/examples/lib/cmdline');
+
+const CardanocliJs = require("./index.js");
+const os = require("os");
+const path = require("path");
+const fs = require('fs');
+// Thumbnail
+const sharp = require('sharp');
+
+const ProposalJs = require("./proposal.js");
+//UUID
+const { v4: uuidv4 } = require("uuid");
+// Crypto library
+const crypto = require('crypto');
+
+// Global variables from credential.json and config.json
+const credentials = require('./config/credentials.json')
+const credentialsAWS = credentials['aws_credentials']
+const credentialsPinata = credentials['pinata_credentials']
+const config = require('./config/config.json')
+const configCardanoCli = config['cardano_cli']
+const configAWSIoTDevice = config['aws_iot_device']
+const configLocalFiles = config['local_files']
+
+// Cardano CLI instance
+const dir = path.join(os.homedir(), configCardanoCli.cardano_node);
+const shelleyPath = path.join(
+  os.homedir(),
+  configCardanoCli.cardano_node,
+  configCardanoCli.testnet_shelley_genesis_json
+);
+
+const cardanocliJs = new CardanocliJs({
+  network: configCardanoCli.network,
+  dir: dir,
+  shelleyGenesisPath: shelleyPath,
+  socketPath: configCardanoCli.socketPath,
+});
+
+// Piñata instance
+const pinata = pinataSDK(credentialsPinata.access_key_id, credentialsPinata.secret_access_key);
+
+
 //begin module
 
 function processTest(args) {
