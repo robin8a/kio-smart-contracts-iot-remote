aa1f90e4bef0219d11fa248ef1cb704fd6e85064
ROBIN OCHOA <robin8a@local.mac>
2021-09-01 14:36:11 -0500

    app.js ready to test IoT

---
+++ startApp.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# stop script on error
+set -e
+
+# Check to see if root CA file exists, download if not
+if [ ! -f ./certs/root-CA.crt ]; then
+  printf "\nDownloading AWS IoT Root CA certificate from AWS...\n"
+  curl https://www.amazontrust.com/repository/AmazonRootCA1.pem > root-CA.crt
+fi
+
+# install AWS Device SDK for NodeJS if not already installed
+if ! node -e "require('aws-iot-device-sdk')" &> /dev/null; then
+  printf "\nInstalling AWS SDK...\n"
+  npm install aws-iot-device-sdk
+fi
+
+# run pub/sub sample app using certificates downloaded in package
+printf "\nRunning pub/sub sample application...\n"
+
+
+## Run modified app.js 
+node app.js \
+--host-name=az6wto8a6h0jn-ats.iot.us-east-1.amazonaws.com \
+--private-key=certs/kio-smart-contracts-iot-device/kio-smart-contracts-iot-device.private.key \
+--client-certificate=certs/kio-smart-contracts-iot-device/kio-smart-contracts-iot-device.cert.pem \
+--ca-certificate=certs/kio-smart-contracts-iot-device/root-CA.crt \
+--client-id=sdk-nodejs-e57c917f-032c-4778-8184-69116bc19f76
